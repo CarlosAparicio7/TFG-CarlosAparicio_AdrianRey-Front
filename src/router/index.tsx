a20cd9fe8fc292a4fc0623bc6ab47c0144b88376
CarlosAparicio7 <ninjacar04@gmail.com>
2026-04-12 20:11:35 +0200

    Empece la funcion en todas las páginas para que me saliera automatico en el router y hice el router con los path de todas las paginas

---
+++ src/router/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import PageNotFound from "../pages/PageNotFound";
+import PeliculaEspecifica from "../pages/PeliculaEspecifica";
+import Register from "../pages/Register";
+import SubirPelicula from "../pages/SubirPelicula";
+
+export const router = createBrowserRouter([
+    {path: "*", element: <PageNotFound/>},
+    {path: "/", element: <Home/>},
+    {path: "/login", element: <Login/>},
+    {path: "/register", element: <Register/>},
+    {path: "/pelicula/:id", element: <PeliculaEspecifica/>},
+    {path: "/subirPelicula", element: <SubirPelicula/>}
+])
