7c70b7f270bc0df6d1add6fbb6ec947eac504651
CarlosAparicio7 <ninjacar04@gmail.com>
2026-04-14 23:15:41 +0200

    PageNotFound hecho y Home empezado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
+type verPeliculas = {
+    id: number,
+    nombre: string,
+    descripcion: string,
+    valoracion: number,
+    archivoVideo: number
+}
+
 export default function Home() {
+    const[usePeliculas, setPeliculas] = useState<verPeliculas[]>([]);
     return (
         <></>
     );
